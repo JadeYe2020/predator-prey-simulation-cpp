e37c6a0dac8276340a52bc3a4c0dd60dc4c50c6f
Jade Ye <JadeYe2020>
2021-09-12 11:11:51 -0300

    Demo in M1U1 video.

---
+++ DemoIORanLoop/cmake-build-debug/CMakeFiles/DemoIORanLoop.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoIORanLoop.dir/main.cpp.o"
+  "DemoIORanLoop.exe"
+  "DemoIORanLoop.pdb"
+  "libDemoIORanLoop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemoIORanLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

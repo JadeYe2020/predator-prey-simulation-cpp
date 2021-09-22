814833f5117e61792fef515ff85d0c1e297dd508
Jade Ye <JadeYe2020>
2021-09-22 11:06:04 -0300

    Initial commit.

---
+++ ValidateNum/cmake-build-debug/CMakeFiles/ValidateNum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ValidateNum.dir/main.cpp.obj"
+  "ValidateNum.exe"
+  "ValidateNum.exe.manifest"
+  "ValidateNum.pdb"
+  "libValidateNum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ValidateNum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

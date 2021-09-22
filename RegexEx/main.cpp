bc9fd5ffe0ea3d130933a5fd3a3e17be1649909c
Jade Ye <JadeYe2020>
2021-09-22 11:34:45 -0300

    Final commit.

--- RegexEx/main.cpp
+++ RegexEx/main.cpp
@@ -6,6 +6,26 @@
 using namespace std;
 
 int main() {
+    string input;
+    regex exp("(\\+|-)?[[:digit:]]+");
+    //if input is correct ask for another number
+    while(true){
+        cout << "Enter the input or q to exit:";
+        cin >> input;
+
+        if (!cin){  //if no input
+            break;
+        }
+        if (input == "q"){
+            break;
+        }
+        if (regex_match(input, exp)){
+            cout << "Input is an integer" << endl;
+        }
+        else {
+            cout << "Input is NOT valid" << endl;
+        }
+    }//end while
 
     return 0;
 }

20ad9e77a486ba482581624b3ea5f9acdaae16f6
Jade Ye <JadeYe2020>
2021-09-22 11:04:27 -0300

    final commit.

---
+++ ValidationEx/cmake-build-debug/CMakeFiles/ValidationEx.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ValidationEx.dir/main.cpp.obj"
+  "ValidationEx.exe"
+  "ValidationEx.exe.manifest"
+  "ValidationEx.pdb"
+  "libValidationEx.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ValidationEx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

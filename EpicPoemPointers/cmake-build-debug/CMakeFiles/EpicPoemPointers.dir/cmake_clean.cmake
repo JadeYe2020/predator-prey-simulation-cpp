ca659ac840893047e2a3f2de91dc5496947767c4
Jade Ye <JadeYe2020>
2021-09-28 11:33:26 -0300

    Demo of EpicPoemPointers

---
+++ EpicPoemPointers/cmake-build-debug/CMakeFiles/EpicPoemPointers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EpicPoemPointers.dir/main.cpp.obj"
+  "EpicPoemPointers.exe"
+  "EpicPoemPointers.exe.manifest"
+  "EpicPoemPointers.pdb"
+  "libEpicPoemPointers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EpicPoemPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

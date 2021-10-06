24141bd66eab70e6d8700dd3279f7a9c319eac25
Jade Ye <JadeYe2020>
2021-10-06 20:33:03 -0300

    Demo followed along.

---
+++ FileIO/cmake-build-debug/CMakeFiles/FileIO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileIO.dir/main.cpp.obj"
+  "FileIO.exe"
+  "FileIO.exe.manifest"
+  "FileIO.pdb"
+  "libFileIO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileIO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

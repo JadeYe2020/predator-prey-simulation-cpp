66633d757d1c1d181af4e5c210c5b8cbb4438109
Jade Ye <JadeYe2020>
2021-10-07 15:13:10 -0300

    Create storeLine() method.

--- Assignmets/Conversion/main.cpp
+++ Assignmets/Conversion/main.cpp
@@ -4,12 +4,40 @@
 
 using namespace std;
 
+string storeLine(string fileName, int lineNum);
+
 int main() {
     string fileName;
 
     cout << "Please enter the file name to convert. e.g. 'c:\\bobFile'" << endl;
     getline(cin, fileName);
-    cout << fileName << endl;
+
+    //NEED A REGEX HERE TO VALIDATE
+
+    cout << storeLine(fileName, 3) << endl;
 
     return 0;
+}
+
+string storeLine(string fileName, int lineNum) {
+    string line;
+    ifstream fileIn;
+
+    fileIn.open(fileName);
+
+    if(!fileIn.fail()) {
+        int lineCount = 0;
+
+        while (!fileIn.eof() || lineCount < lineNum) {
+            getline(fileIn, line);
+
+            lineCount++;
+        }
+
+        return line;
+    }
+    else {
+        cout << "Failed to open the file." << endl;
+        return "";
+    }
 }

0ba33ef4144b8d5d1f02b77a3fcb7e69ad9aa551
Jade Ye <JadeYe2020>
2021-10-14 16:26:14 -0300

    Add more try/catch exception blocks. Finish coding.

--- Assignmets/Conversion/main.cpp
+++ Assignmets/Conversion/main.cpp
@@ -11,10 +11,19 @@ void writeNew(string outFileName, string line);
 void appendLine(string outFileName, string line);
 string findAndReplace(string line, string oldStr, string newStr);
 
+//Create a struct for the programmer defined exception
+struct MyException : public exception
+{
+    const string what()
+    {
+        return "Output file failed to open. Nothing got appended.";
+    }
+};
+
 int main() {
     string inFileName;
     cout << "First, choose a cpp file to convert." << endl;
-    inFileName = askForFilename()+".cpp";
+    inFileName = askForFilename()+".cpp";   //append the file type to complete the filename for the user
 
     string outFileName;
     cout << "And now, decide what html file you want to save it as." << endl;
@@ -22,31 +31,57 @@ int main() {
 
     ifstream fileIn;
     string lineRead;
-
     try
     {
         fileIn.open(inFileName); //try to open the file
-        if(!fileIn.fail()) {
-            //Start writing the out file with the first <PRE> tag
-            writeNew(outFileName, "<PRE>");
+
+        if(fileIn.fail()) {
+            throw exception();
+        }
+        else {
+            try { //try to start writing the out file with the first <PRE> tag
+                writeNew(outFileName, "<PRE>");
+            }
+            catch (const char* msg){
+                cout << msg << endl;
+            }
+            catch (exception& e) {
+                cout << "An unknown error happened" << endl;
+            }
 
             while(!fileIn.eof()){
                 getline(fileIn, lineRead);
-                //convert the line
+
+                //convert the chevrons line by line
                 string lineToWrite = findAndReplace(lineRead, "<", "&lt;");
                 lineToWrite = findAndReplace(lineToWrite, ">", "&gt;");
-                //copy the line into fileOut
-                appendLine(outFileName, lineToWrite);
-            }
+
+                try { //try to copy the line into fileOut
+                    appendLine(outFileName, lineToWrite);
+                }
+                catch (MyException& myE) {
+                    cout << myE.what() << endl;
+                    break;
+                }
+                catch (exception& e) {
+                    cout << "An unknown error happened" << endl;
+                    break;
+                }
+            } //end while
             fileIn.close();
 
-            //lastly, add the end PRE tag.
-            appendLine(outFileName, "</PRE>");
-            cout << "Congratulations! The file has been converted." << endl;
-        }
-        else {
-            throw exception();
-        }
+            try { //lastly, try to add the end PRE tag.
+                appendLine(outFileName, "</PRE>");
+
+                cout << "Congratulations! The file has been converted." << endl;
+            }
+            catch (MyException& myE) {
+                cout << myE.what() << endl;
+            }
+            catch (exception& e) {
+                cout << "An unknown error happened" << endl;
+            }
+        }//end else
     }
     catch (exception& e) {
         cout << "Input file failed to open." << endl;
@@ -58,6 +93,7 @@ int main() {
 string askForFilename() {
     string fileName;
 
+    //Ask the user to enter the file name as the example
     cout << "Please enter the file name. e.g. 'c:\\bobFile'" << endl;
     getline(cin, fileName);
 
@@ -67,6 +103,8 @@ string askForFilename() {
 
     if(!regex_match(fileName, pattern)) {
         cout << "The file path is not valid." << endl;
+        //if the filename is apparently illegal, then call the function itself again to ask the user to re-enter another one
+        // until an valid filename is input.
         fileName = askForFilename();
     }
     else
@@ -75,26 +113,28 @@ string askForFilename() {
 
 void writeNew(string outFileName, string line) {
     ofstream  fileOut;
-    fileOut.open(outFileName);
+    fileOut.open(outFileName);  //not for appending
 
-    if(!fileOut.fail()){
+    if(fileOut.fail()){
+        throw "Output file failed to open. Nothing got written.";
+    }
+    else {
         fileOut << line << endl;
         fileOut.close();
     }
-    else
-        cout << "Output file failed to open." << endl;
 }
 
 void appendLine(string outFileName, string line) {
     ofstream  fileOut;
     fileOut.open(outFileName, ios::app); //try to open the file to append
 
-    if(!fileOut.fail()){
+    if(fileOut.fail()) {
+        throw MyException();
+    }
+    else {
         fileOut << line << endl;
         fileOut.close();
     }
-    else
-        cout << "Output file failed to open." << endl;
 }
 
 string findAndReplace(string line, string oldStr, string newStr)

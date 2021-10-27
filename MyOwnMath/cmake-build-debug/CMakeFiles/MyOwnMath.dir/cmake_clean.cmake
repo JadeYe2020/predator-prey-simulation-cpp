09016e545e152d9c0fb05ddd914a4f09cc393afb
Jade Ye <JadeYe2020>
2021-10-27 19:36:00 -0300

    Demo

---
+++ MyOwnMath/cmake-build-debug/CMakeFiles/MyOwnMath.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyOwnMath.dir/CMakeFiles/MyOwnMath.cpp.obj"
+  "CMakeFiles/MyOwnMath.dir/CMakeFiles/main.cpp.obj"
+  "MyOwnMath.exe"
+  "MyOwnMath.exe.manifest"
+  "MyOwnMath.pdb"
+  "libMyOwnMath.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyOwnMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

525316234906989d55b46483eed4fa9c3a08ecc5
Jade Ye <JadeYe2020>
2021-10-27 12:00:11 -0300

    Demo

---
+++ KnightVsDragon/cmake-build-debug/CMakeFiles/KnightVsDragon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KnightVsDragon.dir/main.cpp.obj"
+  "KnightVsDragon.exe"
+  "KnightVsDragon.exe.manifest"
+  "KnightVsDragon.pdb"
+  "libKnightVsDragon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KnightVsDragon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

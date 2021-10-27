08f71466d6af9efd9311a702fa3675deac6e88a0
Jade Ye <JadeYe2020>
2021-10-27 11:14:40 -0300

    Demo

---
+++ MyCharlieCat/cmake-build-debug/CMakeFiles/MyCharlieCat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCharlieCat.dir/main.cpp.obj"
+  "MyCharlieCat.exe"
+  "MyCharlieCat.exe.manifest"
+  "MyCharlieCat.pdb"
+  "libMyCharlieCat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCharlieCat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

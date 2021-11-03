005a0f33db1d5f3716d0f2ec0977ef5821f90be1
Jade Ye <JadeYe2020>
2021-11-03 13:29:00 -0300

    Demo

---
+++ MyOwnStringClass/cmake-build-debug/CMakeFiles/MyOwnStringClass.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyOwnStringClass.dir/main.cpp.obj"
+  "MyOwnStringClass.exe"
+  "MyOwnStringClass.exe.manifest"
+  "MyOwnStringClass.pdb"
+  "libMyOwnStringClass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyOwnStringClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MyOwnStringClass/main.cpp
@@ -0,0 +1,79 @@
+//Custom String Class - demo of rule of three
+
+#include <iostream>
+#include <ostream>
+#include <cstring>
+using namespace std;
+
+class MyString
+{
+    char* str;
+
+    public:
+        //no arg constructor aka default
+        MyString();
+        //constructor with one argument
+        MyString(char* val);
+        //Copy constructor
+        MyString(const MyString& source);
+        //Move constructor
+        MyString(MyString&& source);
+        //Destructor
+        ~MyString(){cout << "Destructor Fired" << endl; delete []str;};
+
+}; //end Class MyString
+
+//Implementations for class
+//constructor no args
+MyString::MyString()
+    :str{nullptr} //initialization section
+{
+    cout << "Default Constructor Fired" << endl;
+    str = new char[1];
+    str[0] = '\0';
+}
+
+//Constructor with one char* arg
+MyString::MyString(char* val) {
+    cout << "Constructor with char* arg Fired" << endl;
+    if(val == nullptr){
+        str = new char[1];
+        str[0] = '\0';
+    }
+    else{
+        str = new char[strlen(val)+1]; //+1 for null terminating character
+        strcpy(str, val);
+        cout << "The string passed is: " << str << endl;
+    }
+}
+
+//copy constructor
+MyString::MyString(const MyString& source)
+{
+    cout << "Copy Constructor Fired" << endl;
+    str = new char[strlen(source.str)+1];
+    strcpy(str, source.str);
+}
+
+//Move Constructor
+MyString::MyString(MyString&& source)
+{
+    cout << "Move Constructor Fired" << endl;
+    str = source.str;
+    source.str = nullptr;
+}
+
+int main() {
+    //demo default
+    MyString a;
+    //demo one arg constructor
+    MyString b("Hello");
+    //demo copy constructor
+    MyString c(a);
+    char temp[] = "World";
+
+    //demo move constructor
+    MyString d{MyString{temp}};
+
+    return 0;
+} //end main

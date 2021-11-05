6ca3d158b6f94f80df540e52912f5da9b0d94b60
Jade Ye <JadeYe2020>
2021-11-04 22:14:37 -0300

    Finish coding.

--- Assignmets/Courses/main.cpp
+++ Assignmets/Courses/main.cpp
@@ -5,63 +5,82 @@
 using namespace std;
 
 int main() {
-    Student st1; //instantiate 1st student using the default constructor
-    string nameOne, nameTwo, nameThree;
+    string restart; //to save user's input about whether to restart again.
 
-    cout << "Enter student #1's name:";
-    getline(cin, nameOne);
-    //update the name in the Student object
-    st1.setName(nameOne);
+    do {
+        cout << "\nStudent Class Testing" << endl;
+        cout << "Begin testing." << endl;
 
-    cout << "Enter course names for student #1. Type 'Q' when done." << endl;
-    do
-    {
-        string coursName;
+        Student st1; //instantiate 1st student using the default constructor
+        string nameOne, nameTwo, nameThree;
 
-        cout << "Course name:";
-        getline(cin, coursName);
+        cout << "Enter student #1's name:";
+        getline(cin, nameOne);
+        //update the name in the Student object
+        st1.setName(nameOne);
 
-        if(coursName != "Q")
+        cout << "Enter course names for student #1. Type 'Q' when done." << endl;
+        do
         {
-            st1.addCourse(coursName);
-        }
-        else
-            break;
-    } while(true);
+            string coursName;
 
-    //Display the details of 1st student
-    cout << "\nStudent #1's information: " << endl;
-    cout << st1;
+            cout << "Course name:";
+            getline(cin, coursName);
 
-    //Ask for the name of student #2
-    cout << "Enter student #2's name:";
-    getline(cin, nameTwo);
-    //copy the 1st student's course info to create the 2nd student object
-    Student st2(st1);
-    //set the name using the input value
-    st2.setName(nameTwo);
+            if(coursName != "Q")
+            {
+                st1.addCourse(coursName);
+            }
+            else
+                break;
+        } while(true);
 
-    cout << "Student #2's information - obtained with the copy constructor:" << endl;
-    cout << st2;
+        //Display the details of 1st student
+        cout << "\nStudent #1's information: " << endl;
+        cout << st1;
 
-    cout << "Student #1's information after being reset: " << endl;
-    //reset st1
-    st1.reset();
-    cout << st1;
+        //Ask for the name of student #2
+        cout << "Enter student #2's name:";
+        getline(cin, nameTwo);
+        //copy the 1st student's course info to create the 2nd student object
+        Student st2(st1);
+        //set the name using the input value
+        st2.setName(nameTwo);
 
-    cout << "Student #2's information again - an example of a deep copy: " << endl;
-    cout << st2;
+        cout << "Student #2's information - obtained with the copy constructor:" << endl;
+        cout << st2;
 
-    Student st3; //instantiate the 3rd Student object
-    //Ask for the name
-    cout << "Enter the third student name:";
-    getline(cin, nameThree);
-    st3.setName(nameThree);
+        cout << "Student #1's information after being reset: " << endl;
+        //reset st1
+        st1.reset();
+        cout << st1;
 
-    //Copy the course info from st2 using the assignment operator
-    st3 = st2;
-    cout << "\nStudent #3's information - obtained with the assignment operator:" << endl;
-    cout << st3;
+        cout << "Student #2's information again - an example of a deep copy: " << endl;
+        cout << st2;
+
+        Student st3; //instantiate the 3rd Student object
+        //Ask for the name
+        cout << "Enter student #3's name:";
+        getline(cin, nameThree);
+        st3.setName(nameThree);
+
+        //Copy the course info from st2 using the assignment operator
+        st3 = st2;
+        cout << "\nStudent #3's information - obtained with the assignment operator:" << endl;
+        cout << st3;
+
+        cout << "Student #2's information after being reset: " << endl;
+        //reset st2
+        st2.reset();
+        cout << st2;
+
+        cout << "Student #3's information after Student #2 was reset: " << endl;
+        cout << st3;
+
+        cout << "End testing." << endl;
+        cout << "\nEnter 'Y' to restart, or any other key to exit:";
+        getline(cin, restart);
+    } while(restart == "Y");
 
     return 0;
 }

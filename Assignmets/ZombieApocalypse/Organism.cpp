0f43fbaecb75a961623ac940225d2e10cdbbb079
Jade Ye <JadeYe2020>
2021-11-28 21:33:48 -0400

    Implement overloaded << operator method.

--- Assignmets/ZombieApocalypse/Organism.cpp
+++ Assignmets/ZombieApocalypse/Organism.cpp
@@ -2,14 +2,23 @@
 // Created by JadeYe on 11/28/2021.
 //
 #include <iostream>
+#include <windows.h>
+#include "GameSpecs.h"
 #include "Organism.h"
 using namespace std;
 
 class City;
 
+void Col(int c)
+{
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, c);
+}
+
 Organism::Organism():x(0), y(0), moved(false) {
     city = NULL;
-    species = NULL;
+    species = '\0';
 }
 
 Organism::Organism( City *city):x(0), y(0), moved(false)
@@ -32,6 +41,20 @@ void Organism::endTurn()
 
 ostream& operator<<( ostream &output, Organism *organism )
 {
-    output << "-";
+    switch (organism->species) {
+        case 'H':
+            Col(HUMAN_COLOR);
+            output << "H";
+            break;
+        case 'Z':
+            Col(ZOMBIE_COLOR);
+            output << "Z";
+            break;
+        default:
+            Col(DEFAULT_COLOR);
+            output << "-";
+            break;
+    }
+
     return output;
 }

512166c6dd3bdc22a8a0c782bb222c9747bacb20
Jade Ye <JadeYe2020>
2021-11-28 22:23:19 -0400

    1st version of implements

--- Assignmets/ZombieApocalypse/City.cpp
+++ Assignmets/ZombieApocalypse/City.cpp
@@ -6,21 +6,26 @@
 #include "GameSpecs.h"
 #include "City.h"
 #include "Organism.h"
+#include "Human.h"
+#include "Zombie.h"
 
 using namespace std;
 
 class Organism;
+class Human;
+class Zombie;
 
 City::City() {
-    for(int i=0; i<GRIDSIZE; i++) {
-        for(int j=0; j<GRIDSIZE; j++) {
-            Organism *ogn;
-            grid[i][j] = ogn;
-        }
-    }
+//    for(int i=0; i<GRIDSIZE; i++) {
+//        for(int j=0; j<GRIDSIZE; j++) {
+//            Human *ogn = new Human;
+//            grid[i][j] = (Organism*) ogn;
+//        }
+//    }
 }
 
 City::~City(){
+    //source: https://stackoverflow.com/questions/30720594/deleting-a-dynamically-allocated-2d-array/30720628
     for(int i=0; i<GRIDSIZE; i++) {
         for(int j=0; j<GRIDSIZE; j++) {
             delete [] grid[i][j];
@@ -32,21 +37,31 @@ City::~City(){
     cout << "{City's destructor fired}" << endl;
 }
 
-Organism City::*getOrganism( int x, int y )
+Organism* City::getOrganism( int x, int y )
 {
-
+    return grid[x][y];
 }
 
 void City::setOrganism( Organism *organism, int  x, int y )
 {
-
+    grid[x][y] = organism;
 }
 
 void City::move()
 {
-
+    for(int i=0; i<GRIDSIZE; i++) {
+        for(int j=0; j<GRIDSIZE; j++) {
+            grid[i][j]->move();
+        }
+    }
 }
 
 ostream& operator<<( ostream &output, City &city ){
+    for(int i=0; i<GRIDSIZE; i++) {
+        for(int j=0; j<GRIDSIZE; j++) {
+            output << city.grid[i][j];
+        }
+    }
 
+    return output;
 }

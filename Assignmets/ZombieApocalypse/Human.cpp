4f1bf1673c450cb4502e0642e6c66bc109f7cfe4
Jade Ye <JadeYe2020>
2021-11-28 21:35:27 -0400

    1st version of implements

--- Assignmets/ZombieApocalypse/Human.cpp
+++ Assignmets/ZombieApocalypse/Human.cpp
@@ -11,24 +11,27 @@ class City;
 
 Human::Human()
 {
-
+    species = 'H';
 }
 
 Human::Human( City *city )
 {
-
+    species = 'H';
 }
 
 Human::~Human()
 {
-
+    if(city != NULL) //non-null test
+        delete []city;
+    city = NULL; //assign null
 }
 
 void Human::move()
 {
-
+    x -= 1;
+    y -= 1;
 }
 
 char Human::getSpecies(){
-
+    return species;
 }
--- Assignmets/ZombieApocalypse/Zombie.cpp
+++ Assignmets/ZombieApocalypse/Zombie.cpp
@@ -11,25 +11,28 @@ class City;
 
 Zombie::Zombie()
 {
-
+    species = 'Z';
 }
 
 Zombie::Zombie( City *city )
 {
-
+    species = 'Z';
 }
 
 Zombie::~Zombie()
 {
-
+    if(city != NULL) //non-null test
+        delete []city;
+    city = NULL; //assign null
 }
 
 void Zombie::move()
 {
-
+    x += 1;
+    y += 1;
 }
 
 char Zombie::getSpecies()
 {
-
+    return species;
 }

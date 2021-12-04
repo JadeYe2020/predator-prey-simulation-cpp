b90af197c58e0ceefdb8d43301aadf5df9caee9c
Jade Ye <JadeYe2020>
2021-12-03 23:49:46 -0400

    Reset gridsize and modify some char and color values.

--- Assignmets/ZombieApocalypse/GameSpecs.h
+++ Assignmets/ZombieApocalypse/GameSpecs.h
@@ -5,14 +5,14 @@
 #ifndef ZOMBIEAPOCALYPSE_GAMESPECS_H
 #define ZOMBIEAPOCALYPSE_GAMESPECS_H
 
-const int GRIDSIZE = 6; // size of the square grid
+const int GRIDSIZE = 20; // size of the square grid
 const int HUMAN_STARTCOUNT = 100; // inital Humans
 const int ZOMBIE_STARTCOUNT = 5; //initial Zombie count
 const int HUMAN_BREED = 3; // steps until an Human breeds
 const int ZOMBIE_BREED = 8; // steps until an Zombie breeds
 const int ZOMBIE_STARVE = 3; // steps until a Zombie starves and converts back
-const char HUMAN_CH = 72;//79 "o"// ascii Human
-const char SPACE_CH = 61; // " " ascii space
+const char HUMAN_CH = 72;//"H"// ascii Human
+const char SPACE_CH = 45; // "-" ascii space
 const char ZOMBIE_CH = 90;//90 "Z"// ascii zombie
 const int INTERVAL = 900; // pause between steps
 const int ITERATIONS = 1000; // max number of steps
@@ -29,6 +29,6 @@ const int ITERATIONS = 1000; // max number of steps
 //8  = white pale
 const int DEFAULT_COLOR = 0;
 const int HUMAN_COLOR = 3; // turquoise
-const int ZOMBIE_COLOR = 14; // bright yellow
+const int ZOMBIE_COLOR = 12; // bright red
 
 #endif //ZOMBIEAPOCALYPSE_GAMESPECS_H

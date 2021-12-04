5ba5e0f8d96972de552c8132febc7bbe2902b018
Jade Ye <JadeYe2020>
2021-12-04 00:11:49 -0400

    Refactor: create a method to populate the city (to get gen 0).

--- Assignmets/ZombieApocalypse/main.cpp
+++ Assignmets/ZombieApocalypse/main.cpp
@@ -19,36 +19,14 @@ void ClearScreen()
     cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
+void popCity(City *city, int numOfH, int numOfZ);
+
 int main() {
 
     City *city = new City();
+    //call a customized method to populate the city with set numbers of Humans and Zombies
+    popCity(city, HUMAN_STARTCOUNT, ZOMBIE_STARTCOUNT);
 
-    vector<Organism*> vOrg(GRIDSIZE * GRIDSIZE - HUMAN_STARTCOUNT - ZOMBIE_STARTCOUNT); //should have 8 nullptrs
-    //populate vector: , 9 humans and 4 Zombies
-    for(int i=0; i<HUMAN_STARTCOUNT; i++)
-    {
-        Human *hm = new Human(city);
-        vOrg.push_back((Organism*)hm);
-    }
-    for(int i=0; i<ZOMBIE_STARTCOUNT; i++)
-    {
-        Zombie *zb = new Zombie(city);
-        vOrg.push_back((Organism*)zb);
-    }
-
-    //shuffle
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();//create random seed using system clock
-    shuffle(vOrg.begin(), vOrg.end(), default_random_engine(seed));
-
-    //populate the city with creatures
-    int k = 0;
-    for(int i=0; i<GRIDSIZE; i++) {
-        for(int j=0; j<GRIDSIZE; j++) {
-            if(vOrg[k] != NULL)
-                city->setOrganism(vOrg[k], j, i);
-            k++;
-        }
-    }
 
     cout << *city; //prints city
     int genCount = 0;
@@ -60,7 +38,7 @@ int main() {
 
     chrono:: milliseconds interval(INTERVAL);
 
-//    for(int i=0; i<12; i++) { //while both humans and zombies exist
+//    for(int i=0; i<12; i++) {
     do {
         this_thread::sleep_for(interval);
         ClearScreen();
@@ -76,7 +54,7 @@ int main() {
         cout << "GENERATION " << genCount << "\t";
         cout << "HUMANS: " << numOfH << "\t";
         cout << "ZOMBIES: " << numOfZ << endl;
-    } while( numOfH > 0 && numOfZ > 0 && genCount < ITERATIONS);
+    } while( numOfH > 0 && numOfZ > 0 && genCount < ITERATIONS); //while both humans and zombies exist; no more than 1000 iterations
 
     if(genCount != ITERATIONS)
         cout << "Extinction Event - End Program" << endl;
@@ -84,4 +62,38 @@ int main() {
         cout << "1000 iterations finished - End Program" << endl;
 
     return 0;
+}
+
+void popCity(City *city, int numOfH, int numOfZ) {
+    if (numOfH + numOfZ <= GRIDSIZE * GRIDSIZE)
+    {
+        //create a vector to hold all nullptrs inside the grid first
+        vector<Organism*> vOrg(GRIDSIZE * GRIDSIZE - numOfH - numOfZ);
+        //add into the vector: 100 humans and 5 Zombies
+        for(int i=0; i<HUMAN_STARTCOUNT; i++)
+        {
+            Human *hm = new Human(city);
+            vOrg.push_back((Organism*)hm);
+        }
+        for(int i=0; i<ZOMBIE_STARTCOUNT; i++)
+        {
+            Zombie *zb = new Zombie(city);
+            vOrg.push_back((Organism*)zb);
+        }
+
+        //shuffle the fully populated vector
+        unsigned seed = chrono::system_clock::now().time_since_epoch().count();//create random seed using system clock
+        shuffle(vOrg.begin(), vOrg.end(), default_random_engine(seed));
+
+        //populate the city with Organisms and nullptrs in a random fashion
+        int k = 0;
+        for(int i=0; i<GRIDSIZE; i++) {
+            for(int j=0; j<GRIDSIZE; j++) {
+                if(vOrg[k] != NULL)
+                    city->setOrganism(vOrg[k], j, i);
+                k++;
+            }
+        }
+    }
+
 }

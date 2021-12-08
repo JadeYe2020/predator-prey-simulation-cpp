0095b07c0554a13490601c15e6e8a710c3c07d07
Jade Ye <JadeYe2020>
2021-12-08 11:31:21 -0400

    Clean up the code.

--- Assignmets/ZombieApocalypse/main.cpp
+++ Assignmets/ZombieApocalypse/main.cpp
@@ -9,26 +9,24 @@
 #include "GameSpecs.h"
 #include "Organism.h"
 #include "City.h"
-#include "Human.h"
-#include "Zombie.h"
 
 using namespace std;
+
+void makeCity(City *city, int numOfH, int numOfZ);
 
 void ClearScreen()
 {
     cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
-void popCity(City *city, int numOfH, int numOfZ);
-
 int main() {
-
+    //instantiate a new City on heap
     City *city = new City();
-    //call a customized method to populate the city with set numbers of Humans and Zombies
-    popCity(city, HUMAN_STARTCOUNT, ZOMBIE_STARTCOUNT);
+    //call a customized method to randomly populate the city with set numbers of Humans and Zombies
+    makeCity(city, HUMAN_STARTCOUNT, ZOMBIE_STARTCOUNT);
 
-
-    cout << *city; //prints city
+    //print the generation 0 city
+    cout << *city;
     int genCount = 0;
     int numOfH = city->countType(HUMAN_CH);
     int numOfZ = city->countType(ZOMBIE_CH);
@@ -38,47 +36,50 @@ int main() {
 
     chrono:: milliseconds interval(INTERVAL);
 
-//    for(int i=0; i<12; i++) {
     do {
-        this_thread::sleep_for(interval);
+        this_thread::sleep_for(interval); //pause for 900 milliseconds
         ClearScreen();
 
         city->move(); //includes all actions
 
         city->reset(); //resets moved flags
         genCount ++; //increment the generation counter
+        //count humans and zombies respectively
         numOfH = city->countType(HUMAN_CH);
         numOfZ = city->countType(ZOMBIE_CH);
 
-        cout << *city; //prints city
+        cout << *city; //print the city with its new info
         cout << "GENERATION " << genCount << "\t";
         cout << "HUMANS: " << numOfH << "\t";
         cout << "ZOMBIES: " << numOfZ << endl;
+
     } while( numOfH > 0 && numOfZ > 0 && genCount < ITERATIONS); //while both humans and zombies exist; no more than 1000 iterations
 
     if(genCount != ITERATIONS)
         cout << "Extinction Event - End Program" << endl;
-    else
+    else if(numOfH > 0 || numOfZ > 0)
         cout << "1000 iterations finished - End Program" << endl;
+    else
+        cout << "Extinction Event happens at the 1000th iteration - End Program" << endl;
 
     return 0;
 }
 
-void popCity(City *city, int numOfH, int numOfZ) {
+void makeCity(City *city, int numOfH, int numOfZ) {
     if (numOfH + numOfZ <= GRIDSIZE * GRIDSIZE)
     {
-        //create a vector to hold all nullptrs inside the grid first
+        //create a vector to hold all the spaces (i.e., nullptrs) inside the grid first
         vector<Organism*> vOrg(GRIDSIZE * GRIDSIZE - numOfH - numOfZ);
         //add into the vector: 100 humans and 5 Zombies
         for(int i=0; i<HUMAN_STARTCOUNT; i++)
         {
             Human *hm = new Human(city);
-            vOrg.push_back((Organism*)hm);
+            vOrg.push_back((Organism*)hm); //upcasting
         }
         for(int i=0; i<ZOMBIE_STARTCOUNT; i++)
         {
             Zombie *zb = new Zombie(city);
-            vOrg.push_back((Organism*)zb);
+            vOrg.push_back((Organism*)zb); //upcasting
         }
 
         //shuffle the fully populated vector
@@ -89,11 +90,12 @@ void popCity(City *city, int numOfH, int numOfZ) {
         int k = 0;
         for(int i=0; i<GRIDSIZE; i++) {
             for(int j=0; j<GRIDSIZE; j++) {
+                //if it's an Organism object, set it into the position
                 if(vOrg[k] != NULL)
                     city->setOrganism(vOrg[k], j, i);
+                //otherwise, do nothing and move on to the next item
                 k++;
             }
         }
     }
-
 }

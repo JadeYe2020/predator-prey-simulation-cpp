25968412d026db2e1b341c7538acc075c53faebf
Jade Ye <JadeYe2020>
2021-12-08 10:08:02 -0400

    Refactor: correct the if statements about x

--- Assignmets/ZombieApocalypse/Human.cpp
+++ Assignmets/ZombieApocalypse/Human.cpp
@@ -28,123 +28,42 @@ Human::direction Human::getNextStep() {
     //create a vector to store the available spaces
     vector<direction> emptySp;
 
-    if(x != 0) {
+    if(x != GRIDSIZE - 1) { //when the human is not at the east end, check east
         if(city->getOrganism(x+1, y) == NULL)
             emptySp.push_back(EAST);
     }
 
-    if(x != GRIDSIZE - 1) {
+    if(x != 0) { //when the human is not at the west end, check west
         if(city->getOrganism(x-1, y) == NULL)
             emptySp.push_back(WEST);
     }
 
-    if(y != 0) {
+    if(y != 0) { //when the human is not at the north end, check north
         if(city->getOrganism(x, y-1) == NULL)
             emptySp.push_back(NORTH);
     }
 
-    if(y != GRIDSIZE - 1) {
+    if(y != GRIDSIZE - 1) { //when the human is not at the south end, check south
         if(city->getOrganism(x, y+1) == NULL)
             emptySp.push_back(SOUTH);
     }
 
-//    switch (x) {
-//        case 0: //when the human is at the west end of the city
-//            switch(y) {
-//                case 0: //when the human is at the north-west corner, check only east and south
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//                case GRIDSIZE-1: //when the human is at the south-west corner, check only east and north
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    break;
-//                default:
-//                    //check east, north and south
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//            }
-//            break;
-//        case GRIDSIZE-1: //when the human is at the east end of the city
-//            switch(y) {
-//                case 0: //when the human is at the north-east corner, check only west and south
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//                case GRIDSIZE-1: //when the human is at the south-east corner, check only west and north
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    break;
-//                default: //check west, north and south
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//            }
-//            break;
-//        default:
-//            switch(y) {
-//                case 0: //when the human is at the north end of the city, check only east, west and south
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//                case GRIDSIZE-1: //when at the south end of the city, check only east, west and north
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    break;
-//                default: //check all 4 directions
-//                    if(city->getOrganism(x+1, y) == NULL)
-//                        emptySp.push_back(EAST);
-//                    if(city->getOrganism(x-1, y) == NULL)
-//                        emptySp.push_back(WEST);
-//                    if(city->getOrganism(x, y-1) == NULL)
-//                        emptySp.push_back(NORTH);
-//                    if(city->getOrganism(x+1, y-1) == NULL)
-//                    if(city->getOrganism(x, y+1) == NULL)
-//                        emptySp.push_back(SOUTH);
-//                    break;
-//            }
-//            break;
-//    } // end switch
-
     if(emptySp.size() > 0) {
-        //shuffle vector
+        //shuffle vector when it's not empty
         unsigned seed = chrono::system_clock::now().time_since_epoch().count();
         shuffle(emptySp.begin(), emptySp.end(), default_random_engine(seed));
+        //get the first direction of the shuffled vector
         return emptySp[0];
     }
-    else
+    else //otherwise return STAY
         return STAY;
 }
 
 void Human::recruit() {
+    //randomly pick an available space
     direction next = this->getNextStep();
 
-    if(next != STAY) {
+    if(next != STAY) { //recruit only when there's an available space
         //create a new human
         Human *newH = new Human(city);
         newH->endTurn(); //set the moved value to true so that it cannot move this time
@@ -169,7 +88,7 @@ void Human::recruit() {
         } //end switch
     } //end if(!= STAY)
 
-    //no matter recruit successful or not, reset step counter to zero
+    //no matter recruit successful or not, reset the recruit clock to zero
     this->recruitClock = 0;
 }
 
@@ -190,27 +109,22 @@ void Human::move()
                 city->setOrganism(NULL, x-1, y);
                 break;
             case WEST:
-                //put the human to the new position
                 city->setOrganism(this, x-1, y);
-                //put an empty space to the previous position;
                 city->setOrganism(NULL, x+1, y);
                 break;
             case SOUTH:
-                //put the human to the new position
                 city->setOrganism(this, x, y+1);
-                //put an empty space to the previous position;
                 city->setOrganism(NULL, x, y-1);
                 break;
             case NORTH:
-                //put the human to the new position
                 city->setOrganism(this, x, y-1);
-                //put an empty space to the previous position;
                 city->setOrganism(NULL, x, y+1);
                 break;
-            case STAY:
+            case STAY: //do nothing when there's no available space to move
                 break;
         } //end switch
 
+        //check whether the human reach the time when they can recruit
         if(this->recruitClock == HUMAN_BREED)
             this->recruit();
     } //end of if(!moved)
@@ -219,7 +133,3 @@ void Human::move()
 char Human::getSpecies(){
     return species;
 }
-
-//void Human::getPosition() {
-//
-//}
